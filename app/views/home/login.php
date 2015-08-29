d773ed41481751c172b5a2fe1222235ba3b4cf5a
Ashan1 <ashan.me@gmail.com>
2015-08-29 16:42:12 +0530

    First commit

---
+++ app/views/home/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/main.css">
+    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap.css">
+    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap-theme.css">
+
+
+    <script src="../../../public/javascripts/jquery-1.9.0.min.js" type="text/javascript"></script>
+    <script src="../../../public/javascripts/bootstrap.js" type="text/javascript"></script>
+    <script src="../../../public/javascripts/bootstrap.js" type="text/javascript"></script>
+
+
+    </head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <img class="img-responsive" src="../../../public/images/login_left.png"/>
+        </div>
+        <div class="col-sm-6 login_div">
+            <h3 class="login_h text-center">WELCOME TO <span class="span_cas">CAS</span></h3>
+            <h2 class="login_h text-center">USER LOGIN</h2>
+            <form class="form-horizontal" role="form">
+                <div class="input-group col-sm-offset-1 col-sm-11">
+                    <input type="text" class="form-control" id="email" placeholder="Username">
+                            <span class="input-group-addon">
+                                <i class="glyphicon glyphicon-user"></i>
+                            </span>
+                </div>
+                <div>
+                    <br>
+                </div>
+                <div class="input-group col-sm-offset-1 col-sm-11">
+                    <input type="password" class="form-control" id="pwd" placeholder="Password">
+                            <span class="input-group-addon">
+                                <i class="glyphicon glyphicon-lock"></i>
+                            </span>
+                </div>
+
+                <div class="form-group col-sm-12">
+                    <br>
+                    <div class="col-sm-offset-1 col-sm-11">
+                        <a href="#" ><span class="span_white">Forgot password?</span></a>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-sign pull-right">Sign in </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
