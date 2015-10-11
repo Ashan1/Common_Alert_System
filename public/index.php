fc0e64d12b6390be9592b1edcf25ab369a26714f
pmartz92 <pmartz92@gmail.com>
2015-10-11 14:27:49 -0700

    map tabs added

--- public/index.php
+++ public/index.php
@@ -12,12 +12,33 @@
     </aside>
     <aside class="right-side rgt">
         <div class="container-fluid">
-            <div class="row">
+            <div class="row recent-activity">
 
             </div>
-            <div class="row"></div>
-            <div class="row">
+            <div class="row sub-menu">
 
+            </div>
+            <div class="row summary-circles">
+                <div class="circles col-lg-12">
+                    <div class="col-lg-4 circle01"></div>
+                    <div class="col-lg-4 circle02"></div>
+                    <div class="col-lg-4 circle03"></div>
+                    <div class="col-lg-4 circle04"></div>
+                </div>
+                <div class="col-lg-12 map-menu">
+                    <ul class="nav nav-tabls">
+                        <li role="presentation" class=""><a href="#slmap" aria-controls="slmap" role="tab" data-toggle="tab">Sri Lanka</a></li>
+                        <li role="presentation" class=""><a href="#worldmap" aria-controls="worldmap" role="tab" data-toggle="tab">World</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row maps tab-content">
+                <div role="tabpanel" class="tab-pane fade in active" id="slmap">
+                    <p>Srilanka map goes here</p>
+                </div>
+                <div role="tabpanel" class="tab-pane fade" id="worldmap">
+                    <p>worls map goes here</p>
+                </div>
             </div>
         </div>
     </aside>
