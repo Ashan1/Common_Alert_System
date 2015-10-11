e202f553745fac0668a6ea0bafa70e4b4ba67919
Thushara91 <thusharadahanayaka@gmail.com>
2015-10-12 00:08:56 +0530

    addDB

--- app/views/home/submit.php
+++ app/views/home/submit.php
@@ -21,6 +21,7 @@
 </head>
 <body>
 <?php
+include 'connect.php';
 $nameErr = $emailErr =$nicErr = $titleErr = $addressErr = $mobileErr ="";
 $name = $email =$nic = $title = $address = $mobile ="";
 
@@ -31,53 +32,6 @@ $title = test_input($_POST['formTitle']);
 $mobile = test_input($_POST['formMobile']);
 $address = test_input($_POST['formAddress']);
 
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["formName"])) {
-        $nameErr = "Name is required";
-    } else {
-        $name = test_input($_POST["formName"]);
-        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
-            $nameErr = "Only letters and white space allowed";
-        }
-    }
-    if (empty($_POST["formEmail"])) {
-        $emailErr = "Email is required";
-    } else {
-        $email = test_input($_POST["formEmail"]);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format";
-        }
-    }
-
-
-    if (empty($_POST["formNIC"])) {
-        $nicErr = "NIC is required";
-    } else {
-        $nic = ($_POST["formNIC"]);
-        if (strlen($nic) <> 10) {
-            $nicErr = "Invalid NIC number";
-        }
-    }
-    if (empty($_POST["title"])) {
-        $titleErr = "Title is required";
-    } else {
-        $title = test_input($_POST["formTitle"]);
-    }
-
-    if (empty($_POST["formMobile"])) {
-        $mobileErr = "Mobile Number is Required";
-    } else {
-        $mobile = test_input($_POST["formMobile"]);
-        if ((strlen($mobile) <> 10) && (!preg_match("/^[0-9 ]*$/", $name))) {
-            $mobileErr = "Invalid mobile number";
-        }
-    }
-    if (empty($_POST["address"])) {
-        $addressErr = "";
-    } else {
-        $address = test_input($_POST["formAddress"]);
-    }
-}*/
 function test_input($data)
 {
     $data = trim($data);
@@ -85,7 +39,7 @@ function test_input($data)
     $data = htmlspecialchars($data);
     return $data;
 }
-$mydata = array($name,$email,$nic,$title,$mobile,$address);
+/*$mydata = array($name,$email,$nic,$title,$mobile,$address);
 $myname = array("Name: ","Email: ","NIC: ","Title: ","Mobile: ","Address: ");
 echo "<h2>Your Input:</h2>";
 $arrlength =count($mydata);
@@ -93,9 +47,27 @@ for ($x =0 ;$x < $arrlength; $x++){
     echo $myname[$x];
     echo $mydata[$x];
     echo "<br>";
+}*/
+
+
+
+try {
+    $conn = new PDO("mysql:host=$hostrname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO employee (E_name,E_email,E_NIC,E_title,E_mobile,E_address,E_password)
+VALUES ('$name', '$email','$nic','$title','$mobile','$address','$nic')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
 }
 
+$conn = null;
 ?>
-
+<label type="button" name="back">
 </body>
 </html>
