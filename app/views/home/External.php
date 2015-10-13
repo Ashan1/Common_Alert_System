e02d320c96ede5aad96ab6d7d5154918bca6f077
cutedili <dweerasinghe91@gmail.com>
2015-10-14 01:38:43 +0530

    update_interface

--- app/views/home/External.php
+++ app/views/home/External.php
@@ -1,158 +1,86 @@
-<html>
+<?php include('head.php');?>
+
 <head lang="en">
    <meta charset="UTF-8">
-    <title></title>
+   <title></title>
    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap.css">
-    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap-theme.css">
-    <link rel="stylesheet" media="screen" href="../../../public/stylesheets/menu.css">
-	 <link rel="stylesheet" media="screen" href="../../../public/stylesheets/addusercss.css">
-	  <link href="../../../public/stylesheets/bootstrap.min.css" rel="stylesheet">
-    <link href="../../../public/stylesheets/new.css" rel="stylesheet">
+   <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap-theme.css">
+   <link rel="stylesheet" media="screen" href="../../../public/stylesheets/menu.css">
+   <link rel="stylesheet" media="screen" href="../../../public/stylesheets/addusercss.css">
+   <link href="../../../public/stylesheets/bootstrap.min.css" rel="stylesheet">
+   <link href="../../../public/stylesheets/new.css" rel="stylesheet">
 
-    <script src="../../../public/javascripts/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script src="../../../public/javascripts/bootstrap.js" type="text/javascript"></script>
-<script src="../../../public/javascripts/jquery.min.js"></script>
-  <script src="../../../public/javascripts/popup.min.js"></script>
-	
-   
-
+   <script src="../../../public/javascripts/jquery-1.9.0.min.js" type="text/javascript"></script>
+   <script src="../../../public/javascripts/bootstrap.js" type="text/javascript"></script>
+   <script src="../../../public/javascripts/jquery.min.js"></script>
+   <script src="../../../public/javascripts/popup.min.js"></script>
 </head>
-<body>
 
-<div id="wrapper">
-
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-
-        <div class="logo">
-            <img src="../../../public/images/logo.png">
-        </div>
-        <div class="profile">
-           <div><img src="../../../public/images/pro-pic.png"></div>
-            <div class="pro-name">
-            <ul>
-                <li>Admin</li>
-                <li>Logout</li>
-            </ul>
-            </div>
-        </div>
-
-        <div>
-            <ul class="sidebar-nav">
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Disasters</a>
-                </li>
-                <li>
-                    <a href="#">Alerts</a>
-                </li>
-                <li>
-                    <a href="#">E-mails</a>
-                </li>
-                <li>
-                    <a href="#">Admin Panel</a>
-                </li>
-            </ul>
-        </div>
-
-    </div>
-    <!-- sidebar -->
-
+<div >
+    <aside class="left-side">
+        <?php include('../app/views/home/side.php');?>
+    </aside>
+    <aside class="right-side rgt">
     <!-- Page Content -->
-    <div id="page-content-wrapper" class="page-content-wrapper">
-        <div class="container-fluid main-content">
-            <div class="row">
                 <div class="col-lg-12">
-                    <!--status bar-->
-
-                    <div class="status-bar row">
-                        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 menu-icon">
-                            <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></a>
-                        </div>
-                        <div class="col-lg-8 search">
-                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                            <input type="text" value="Search Disaster"/>
-                        </div>
-                        <div class="col-lg-1 bell"> <span class="glyphicon glyphicon-bell" aria-hidden="true"></span> </div>
-                       <div class="col-lg-2">
-							<?php
-								echo date("F jS")."<br>";
-								date_default_timezone_set("Asia/colombo");
-								echo date("h:i:s A");
-							?>
-						</div>
-
-                    </div>
-
-                    <!--/Status bar-->
-                    <div>
-					
-					
 					<div id="layout">
 	
-<div style="background-color:green;">
-		  
+						<div style="background-color:green;">
+							<div id="recent">
 	 
-	 <div id="recent">
-	 
-	 <h2 style="float:left;">EXTERNAL AUTHORITY DETAILS</h2>
+								<h2 style="float:left;">EXTERNAL AUTHORITY DETAILS</h2>
 		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal"  type="button"  ><img src="../../../public/images/refresh.png" class="div_button_img">Update</button>
-		</div>
-		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal" type="button" ><img src="../../../public/images/delete.png" class="div_button_img">Remove</button>
-		</div>
-		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal" data-target="#myModal" type="button" ><img src="../../../public/images/building.png" class="div_button_img">Add New</button>
-		</div>
-	
-		<div class="modal fade" id="myModal" role="dialog" action="../../../public/php/addnew.php" style="padding-top:100px;" >
-			<div class="modal-dialog" >
-				
-				<div class="row">
-  <div class="col-md-8 col-md-offset-2" style="background-color:black;background: rgba(0, 0, 0, 0.6);height:310px;">
+									<div style="float:right;">
+										<button class="div_button" data-toggle="modal"  type="button"  ><img src="../../../public/images/refresh.png" class="div_button_img">Update</button>
+									</div>
+									<div style="float:right;">
+										<button class="div_button" data-toggle="modal" type="button" ><img src="../../../public/images/delete.png" class="div_button_img">Remove</button>
+									</div>
+									<div style="float:right;">
+										<button class="div_button" data-toggle="modal" data-target="#myModal" type="button" ><img src="../../../public/images/building.png" class="div_button_img">Add New</button>
+									</div>
+									<div class="modal fade" id="myModal" role="dialog" action="../../../public/php/addnew.php" style="padding-top:100px;" >
+										<div class="modal-dialog" >
+											<div class="row">
+												<div class="col-md-8 col-md-offset-2" style="background-color:black;background: rgba(0, 0, 0, 0.6);height:310px;">
   
-	<h4 style="color:white;text-align:left;padding-top: 10px;padding-bottom:10px;">ADD EXTERNAL AUTHORITY</h4>
-           <form class="form-horizontal" action="../../../public/php/external_auth.php"  method="post">
-        <div class="form-group">
-            <div class="col-xs-10">
-                <input type="name" name="auth_name" class="form-control" align="center"style="margin-left: 40px;"  placeholder="Name">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-xs-10">
-                <input type="tel" name="auth_tel" class="form-control" align="center"style="margin-left: 40px;" id="inputEmail" placeholder="Telphone Number">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-xs-10">
-                <input type="address" name="auth_address" class="form-control" align="center"style="margin-left: 40px;" placeholder="Address">
-            </div>
-        </div>
-		<div class="form-group">
-            <div class="col-xs-10">
-                <input type="email" class="form-control" name="auth_email" align="center"style="margin-left: 40px;" placeholder="Email Address">
-            </div>
-        </div>
-		 <div class="form-group">
-            <div class="col-xs-offset-2 col-xs-10">
-                <button type="Submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal1" name="formSubmit" onClick="f_validateForm()" value="Submit" Style="background-color:#FF4000; fload:right;margin-left: 240px;margin-bottom:15px;">Add</button>
-			</div>		
-         </div>
-    </form>
-        </div>
-		</div>
-  </div>
-      
-    </div>
+												<h4 style="color:white;text-align:left;padding-top: 10px;padding-bottom:10px;">ADD EXTERNAL AUTHORITY</h4>
+													
+													<form class="form-horizontal" action="../../../public/php/external_auth.php"  method="post">
+														<div class="form-group">
+															<div class="col-xs-10">
+																<input type="name" name="auth_name" class="form-control" align="center"style="margin-left: 40px;"  placeholder="Name">
+															</div>
+														</div>
+														<div class="form-group">
+															<div class="col-xs-10">
+																<input type="tel" name="auth_tel" class="form-control" align="center"style="margin-left: 40px;" id="inputEmail" placeholder="Telphone Number">
+															</div>
+														</div>
+														<div class="form-group">
+															<div class="col-xs-10">
+																<input type="address" name="auth_address" class="form-control" align="center"style="margin-left: 40px;" placeholder="Address">
+															</div>
+														</div>
+														<div class="form-group">
+															<div class="col-xs-10">
+																<input type="email" class="form-control" name="auth_email" align="center"style="margin-left: 40px;" placeholder="Email Address">
+															</div>
+														</div>
+														<div class="form-group">
+															<div class="col-xs-offset-2 col-xs-10">
+																<button type="Submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal1" name="formSubmit" onClick="f_validateForm()" value="Submit" Style="background-color:#FF4000; fload:right;margin-left: 240px;margin-bottom:15px;">Add</button>
+															</div>		
+														</div>
+													</form>
+												</div>
+											</div>
+										</div>
+									</div>
 
-  </div> <!--recent ends-->
-</div>	 	
+							</div> <!--recent ends-->
+						</div>	 
+						
 	<div id="content" scrolling="yes">
 		 <?php
       include "php/connect.php";
@@ -190,28 +118,11 @@
 	
 	 
 	 
-	</div><!--layout ends-->
-					
-					
-					</div>
-
-                    <!--<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">|||</a>-->
-                </div>
-            </div>
-        </div>
-    </div></div>
-    <!-- /page-content-->
-
-</div>
-
-
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
-
+					</div><!--layout ends-->	
+				</div>
+	</aside>
+			<!--<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">|||</a>-->
+</div><!-- /page-content-->
 
 
 </body>
--- app/views/home/adduser.php
+++ app/views/home/adduser.php
@@ -1,183 +1,112 @@
-<html>
+<?php include('head.php');?>
+
 <head lang="en">
     <meta charset="UTF-8">
     <title></title>
-   <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap.css">
+	<link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap.css">
     <link rel="stylesheet" media="screen" href="../../../public/stylesheets/bootstrap-theme.css">
     <link rel="stylesheet" media="screen" href="../../../public/stylesheets/menu.css">
-	 <link rel="stylesheet" media="screen" href="../../../public/stylesheets/addusercss.css">
-	 <link href="../../../public/stylesheets/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" media="screen" href="../../../public/stylesheets/addusercss.css">
+	<link href="../../../public/stylesheets/bootstrap.min.css" rel="stylesheet">
     <link href="../../../public/stylesheets/new.css" rel="stylesheet">
 
     <script src="../../../public/javascripts/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="../../../public/javascripts/bootstrap.js" type="text/javascript"></script>
 	<script src="../../../public/javascripts/jquery.min.js"></script>
-  <script src="../../../public/javascripts/popup.min.js"></script>
-	
-  
-
+	<script src="../../../public/javascripts/popup.min.js"></script>
 </head>
-<body>
 
-<div id="wrapper">
-
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-
-        <div class="logo">
-            <img src="../../../public/images/logo.png">
-        </div>
-        <div class="profile">
-           <div><img src="../../../public/images/pro-pic.png"></div>
-            <div class="pro-name">
-            <ul>
-                <li>Admin</li>
-                <li>Logout</li>
-            </ul>
-            </div>
-        </div>
-
-        <div>
-            <ul class="sidebar-nav">
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Disasters</a>
-                </li>
-                <li>
-                    <a href="#">Alerts</a>
-                </li>
-                <li>
-                    <a href="#">E-mails</a>
-                </li>
-                <li>
-                    <a href="#">Admin Panel</a>
-                </li>
-            </ul>
-        </div>
-
-    </div>
-    <!-- sidebar -->
-
+<div >
+    <aside class="left-side">
+        <?php include('../app/views/home/side.php');?>
+    </aside>
+    <aside class="right-side rgt">
     <!-- Page Content -->
-    <div id="page-content-wrap per" class="page-content-wrapper">
-        <div class="container-fluid main-content">
-            <div class="row">
                 <div class="col-lg-12">
-                    <!--status bar-->
-
-                    <div class="status-bar row">
-                        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 menu-icon">
-                            <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></a>
-                        </div>
-                        <div class="col-lg-8 search">
-                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                            <input type="text" value="Search Disaster"/>
-                        </div>
-                        <div class="col-lg-1 bell"> <span class="glyphicon glyphicon-bell" aria-hidden="true"></span> </div>
-						
-                        <div class="col-lg-2">
-							<?php
-								echo date("F jS")."<br>";
-								date_default_timezone_set("Asia/colombo");
-								echo date("h:i:s A");
-							?>
-						</div>
-
-                    </div>
-
-                    <!--/Status bar-->
-                    <div>
-					
-					
-					<div id="layout">
+                    <div id="layout">
 	
-	<div style="background-color:green;">
-		  
+						<div style="background-color:green;">
+							<div id="recent">
 	 
-	 <div id="recent">
-	 
-	 <h2 style="float:left;">USER ACCOUNTS</h2>
+								<h2 style="float:left;">USER ACCOUNTS</h2>
 		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal"  type="button"  ><img src="../../../public/images/refresh.png" class="div_button_img">Update</button>
-		</div>
+								<div style="float:right;">
+									<button class="div_button" data-toggle="modal"  type="button"  ><img src="../../../public/images/refresh.png" class="div_button_img">Update</button>
+								</div>
 		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal" type="button" ><img src="../../../public/images/remove.png" class="div_button_img">Remove</button>
-		</div>
+								<div style="float:right;">
+									<button class="div_button" data-toggle="modal" type="button" ><img src="../../../public/images/remove.png" class="div_button_img">Remove</button>
+								</div>
 		
-		<div style="float:right;">
-			<button class="div_button" data-toggle="modal" data-target="#myModal" type="button" ><img src="../../../public/images/Add.png" class="div_button_img">Add New</button>
-		</div>
+								<div style="float:right;">
+									<button class="div_button" data-toggle="modal" data-target="#myModal" type="button" ><img src="../../../public/images/Add.png" class="div_button_img">Add New</button>
+								</div>
 	
-		<div class="modal fade" id="myModal" role="dialog" action="../../../public/php/addnew.php" >
-		<div class="modal-dialog">
+								<div class="modal fade" id="myModal" role="dialog" action="../../../public/php/addnew.php" >
+									<div class="modal-dialog">
 				
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 model_addnew" >
-				<h4 style="color:white;text-align:left;">ADD USER</h4>
-				<form class="form-horizontal" action="../../../public/php/addnew.php"  method="post">
-					<div class="form-group">
-						<div class="col-xs-10">
-							<input type="name" name="formName" class="form-control modal_input" align="center" placeholder="Name">
+										<div class="row">
+											<div class="col-md-8 col-md-offset-2 model_addnew" >
+												<h4 style="color:white;text-align:left;">ADD USER</h4>
+												<form class="form-horizontal" action="../../../public/php/addnew.php"  method="post">
+													<div class="form-group">
+														<div class="col-xs-10">
+															<input type="name" name="formName" class="form-control modal_input" align="center" placeholder="Name">
+														</div>
+													</div>
+													<div class="form-group">
+														<div class="col-xs-10">
+															<input type="email" name="formEmail" class="form-control modal_input" align="center" placeholder="Email">
+														</div>
+													</div>
+													<div class="form-group">
+														<div class="col-xs-10">
+															<input type="nic" name="formNIC" class="form-control modal_input" align="center" placeholder="NIC number">
+														</div>
+													</div>
+													<div class="form-group">
+														<div class="col-xs-10">
+															<input type="Mobile" class="form-control modal_input" name="formMobile" align="center" placeholder="Mobile number">
+														</div>
+													</div>
+													<div class="form-group">
+														<div class="col-xs-10">
+															<input type="address" class="form-control modal_input" name="formAddress" align="center" placeholder="Adress">
+														</div>
+													</div>
+													<div class="form-group">
+														<div class="col-xs-10"> 
+															<select class="form-control modal_input " name="size" align="center" >
+																<option value="">Role</option>
+																<option value="Administrator">Administrator</option>
+																<option value="General User">General User</option>
+																<option value="Operational User">Operational User</option>
+																<option value="Executive User">Executive User</option>
+															</select>
+														</div>
+													</div>
+													<div>
+														<div class="fileupload fileupload-new" data-provides="fileupload">
+															<div class="fileupload-preview thumbnail" style="width: 100px; height: 100px;margin-left: 40px;">
+																<div style="margin-left: 100px;">
+																	<span class="fileupload-exists" style=" color:white;">Change</span><input type="file" /></span>
+																	<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
+																</div>
+															</div>
+														</div>
+														<div class="form-group">
+															<div class="col-xs-offset-2 col-xs-10">
+																<button type="Submit" class="btn modal_btn" data-toggle="modal" data-target="#myModal1" name="formSubmit" onClick="f_validateForm()" value="Submit">Add</button>
+															</div>		
+														</div>
+													</div>	
+												</form
+											</div>
+										</div>
+									</div>
+								</div>
+							</div> <!--recent ends-->
 						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-10">
-							<input type="email" name="formEmail" class="form-control modal_input" align="center" placeholder="Email">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-10">
-							<input type="nic" name="formNIC" class="form-control modal_input" align="center" placeholder="NIC number">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-10">
-							<input type="Mobile" class="form-control modal_input" name="formMobile" align="center" placeholder="Mobile number">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-10">
-							<input type="address" class="form-control modal_input" name="formAddress" align="center" placeholder="Adress">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-xs-10"> 
-							<select class="form-control modal_input " name="size" align="center" >
-							<option value="">Role</option>
-							<option value="Administrator">Administrator</option>
-							<option value="General User">General User</option>
-							<option value="Operational User">Operational User</option>
-							<option value="Executive User">Executive User</option>
-							</select>
-						</div>
-					</div>
-					<div>
-						<div class="fileupload fileupload-new" data-provides="fileupload">
-							<div class="fileupload-preview thumbnail" style="width: 100px; height: 100px;margin-left: 40px;">
-							<div style="margin-left: 100px;">
-								<span class="fileupload-exists" style=" color:white;">Change</span><input type="file" /></span>
-								<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
-							</div>
-							</div>
-						</div>
-					<div class="form-group">
-						<div class="col-xs-offset-2 col-xs-10">
-							<button type="Submit" class="btn modal_btn" data-toggle="modal" data-target="#myModal1" name="formSubmit" onClick="f_validateForm()" value="Submit">Add</button>
-						</div>		
-					</div>
-    </form>
-        </div>
-		</div>
-  </div>
-      
-    </div>
-  </div>
-  </div> <!--recent ends-->
-  </div>
 	 
    
 		
@@ -186,7 +115,7 @@
       include "php/connect.php";
 
       //execute the SQL query and return records
-      $result = mysql_query("SELECT E_nic, E_name, email, role FROM employee ");
+      $result = mysql_query("SELECT E_nic, E_name, E_email, E_job_role FROM employee ");
       ?>
       
 		<table class="table" >
@@ -207,8 +136,8 @@
             "<tr>
               <td>{$row['E_nic']}</td>
               <td>{$row['E_name']}</td>
-              <td>{$row['email']}</td>
-              <td>{$row['role']}</td>
+              <td>{$row['E_email']}</td>
+              <td>{$row['E_job_role']}</td>
 			  </tr>\n";
           }
         ?>
@@ -219,30 +148,13 @@
 	
 	 
 	 
-	</div><!--layout ends-->
-					
-					
-					
-					
-					</div>
+					</div><!--layout ends-->
+				</div>
 
                     <!--<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">|||</a>-->
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /page-content-->
-
+     <!-- /page-content-->
+	</aside>
 </div>
-
-
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-</script>
-
 
 
 </body>
