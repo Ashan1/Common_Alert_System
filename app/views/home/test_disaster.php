a058541b0ce92636d38a237608ff431f02f9508f
Ashan1 <ashan.me@gmail.com>
2015-10-20 01:01:31 +0530

    Map popup box

--- app/views/home/test_disaster.php
+++ app/views/home/test_disaster.php
@@ -1,65 +1,58 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <style>
-        html, body, #map {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-        }
+        html, body, #map { margin: 0; padding: 0; height: 100%; }
     </style>
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script
+        src="https://maps.googleapis.com/maps/api/js?libraries=visualization">
+    </script>
     <script>
         var map;
 
         function initialize() {
-            var mapOptions = {
+            map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 3,
-                center: {lat: 7.0000, lng: 81.0000},
+                center: {lat: 7.9500, lng: 81.0000},
                 mapTypeId: google.maps.MapTypeId.SATELLITE
-            };
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            });
 
-
+            // Create a <script> tag and set the USGS URL as the source.
             var script = document.createElement('script');
-
-
-            script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojsonp';
-
-
+            // (In this example we use a locally stored copy instead.)
+            script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
             document.getElementsByTagName('head')[0].appendChild(script);
-
-
-            //Get style circle
-            var style = function(feature) {
-                var magnitude = feature.getProperty('mag');
-                return {
-                    icon: getCircle(magnitude)
-                };
-            };
-
-            map.data.setStyle(style);
-
         }
 
-        function getCircle(magnitude) {
-            var circle = {
-                path: google.maps.SymbolPath.CIRCLE,
-                fillColor: 'red',
-                fillOpacity: .2,
-                scale: Math.pow(2, magnitude) / 2,
-                strokeColor: 'white',
-                strokeWeight: .5
-            };
-            return circle;
+        // Loop through the results array and place a marker for each
+        // set of coordinates.
+        var infowindow = new google.maps.InfoWindow({});
+
+        function getHandler(place, mag){
+            return function handler() {
+                infowindow.setContent("<table><tr><td>Place</td><td>" + place + "</td></tr><tr><td>Magnitude</td><td>" + mag + "</td></tr></table>");
+                infowindow.open(map, this);
+            }
         }
 
-        function eqfeed_callback(results) {
-            map.data.addGeoJson(results);
+        window.eqfeed_callback = function(results) {
+            for (var i = 0; i < results.features.length; i++) {
+                var coords = results.features[i].geometry.coordinates;
+                var place = results.features[i].properties.place;
+                var mag = results.features[i].properties.mag;
+                var latLng = new google.maps.LatLng(coords[1],coords[0]);
+                var marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map,
+                    clickable: true
+                });
+                marker.setIcon('https://www.google.com/mapfiles/marker_green.png');
+                google.maps.event.addListener(marker, 'click', getHandler(place, mag));
+            }
         }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+
+        google.maps.event.addDomListener(window, 'load', initialize)
 
     </script>
 </head>
